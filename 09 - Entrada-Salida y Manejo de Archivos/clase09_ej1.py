dbd60ebd1db67d160325c6452578fcc1e496268f
LeoBalleis <leonel.balleis@gmail.com>
2022-08-26 01:47:18 -0300

    tareafinal

--- 09 - Entrada-Salida y Manejo de Archivos/clase09_ej1.py
+++ 09 - Entrada-Salida y Manejo de Archivos/clase09_ej1.py
@@ -1,9 +1,10 @@
 import sys
 # Comprobación de seguridad, ejecutar sólo si se recibe 3 argumentos
 if len(sys.argv) == 4:
-    print("El primer parámetro es:",sys.argv[1])
+    print("El nombre del script es :",sys.argv[0])
     print("El segundo parámetro es:",sys.argv[2])
     print("El tercer parámetro es:",sys.argv[3])
+    print("El primer parámetro es:",sys.argv[1])
 else:
     print("ERROR: Introdujo una cantidad de argumentos distinta de tres (3)")
     print('Ejemplo: clase09_ej1.py 1 2 3')
